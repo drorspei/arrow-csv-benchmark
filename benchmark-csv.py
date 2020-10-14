67e7165e9eb7096af2d3ebf32e751abb46a0b0c0
Dror Speiser <dror.mastershin@gmail.com>
2020-10-14 21:01:33 +0300

    Update benchmark-csv.py

--- benchmark-csv.py
+++ benchmark-csv.py
@@ -1,10 +1,12 @@
-num_gigabytes = 10
-str_columns, str_lens = 200, 13
-float_columns = 200
-null_columns = 20
-num_rounds = 5
-backend = "loky"
-n_jobs = -1
+# Parameters for script.
+# These values reflect an actual work load I had that was slow.
+num_gigabytes = 10  # Approximate number of giga bytes of the csv
+str_columns, str_lens = 200, 13  # How many string columns to include, and what is the uniform length of all strings
+float_columns = 200  # How many float columns to include
+null_columns = 20  # How many all-null columns to include
+num_rounds = 5  # How many calls to read_csv to make
+backend = "loky"  # Joblib backend for parallelising creation of csv
+n_jobs = -1  # Number of jobs for csv creation parallelisation
 
 #####################################################################
 
@@ -27,16 +29,21 @@ def create_csv(
     random_seed, columns_permutation,
     header
     ):
+    """Creates random csv with given parameters"""
+    # Create random string columns
     rand = np.random.RandomState(random_seed)
     A, Z = np.array(["A","Z"]).view("int32")
     strs = rand.randint(
         low=A,high=Z,size=(num_rows, str_columns, str_lens),dtype="int32"
     ).view(f"U{str_lens}")[..., 0]
 
+    # Create random float columns
     floats = rand.randn(num_rows, float_columns)
 
+    # Create null columns
     nulls = [[pd.NA] * null_columns] * num_rows
 
+    # Put columns into a pandas DataFrame in same random order
     df = pd.concat([
         pd.DataFrame(
             values,
@@ -48,25 +55,31 @@ def create_csv(
     ], axis=1)
     df = df.iloc[:, columns_permutation]
 
+    # Create the csv for the DataFrame
     data_raw = df.to_csv(sep="|", header=header).encode()
     return data_raw
 
 if __name__ == "__main__":
-    print("pid:", os.getpid())
+    print("pid:", os.getpid())  # Print pid so I can run py-spy on it
 
+    # Compute a random perutation for the columns order.
     columns_permutation = np.random.RandomState(2).permutation(
         str_columns + float_columns + null_columns
     )
+    
+    # Approximate number of rows needed to fulfill approximate number of gigabytes
     row_gigabyte = (
         (
             str_columns * (str_lens + 1)
-            + float_columns * (19 + 1)
+            + float_columns * (19 + 1)  # The floats have aound 19 characters on average
             + null_columns + 2
         ) * cpu_count() / 1024**3
     )
     num_rows = int(num_gigabytes / row_gigabyte)
+    
     print("1/3 Creating csv chunks")
 
+    # Parallelise creation of csv
     with joblib.Parallel(n_jobs=n_jobs, backend=backend) as pool:
         data_raw = pool(
             create_csv(
@@ -77,6 +90,7 @@ if __name__ == "__main__":
         )
 
     print("2/3 Creating bytes")
+    
     total_size = sum(map(len, data_raw)) / 1024**3
     data_bytes = BytesIO()
     for b in data_raw:
@@ -86,18 +100,22 @@ if __name__ == "__main__":
     parse_options = pyarrow.csv.ParseOptions(delimiter="|")
 
     for i in range(num_rounds):
-        data_bytes.seek(0)
+        data_bytes.seek(0)  # Go back to beginning of buffer
         print(f"starting {i}/{num_rounds}")
+        
+        # Actual call to read_csv
         t1 = time.time()
         a = pyarrow.csv.read_csv(data_bytes, parse_options=parse_options)
         t2 = time.time()
+        
         times.append(t2 - t1)
         print(f"done in {times[-1]:.2f} secs, {total_size / times[-1]:.2f} GiB/s")
         del a
         
     print(
         "processed", total_size,
-        "in", total_size / np.median(times), "GiB/s"
+        "in max speed of", total_size / min(times), "GiB/s"
     )
+    # Print all times
     print(", ".join("%.2f" % v for v in (total_size / np.array(times))))
 
